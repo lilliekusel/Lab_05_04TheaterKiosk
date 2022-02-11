9b4afa07a47b76f7dc0520f12e4b3b3f94b191da
Lillie Kusel <lilliesmacbook@lillies-air.wifi.uc.edu>
2022-02-11 10:12:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int age = 21;
+
+        if (age >= 21);
+        System.out.println("You get a wristband!");
+            
+    }
+}
